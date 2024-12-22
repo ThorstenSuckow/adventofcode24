bc2c8a3df360291caa309a5a5d27bde7f1de3e68
ThorstenSuckow <thorsten@suckow-homberg.de>
2024-12-23 00:39:24 +0100

    refactor: code cleanup and runtime improvements

--- day22/util.py
+++ day22/util.py
@@ -36,94 +36,50 @@ def part2_process(secrets: list) -> int:
     for secret in secrets:
         buyers.append(calc_secret(secret, 2000, True))
 
-    idx = 0
-    for res in buyers:	
+    for idx, res in enumerate(buyers, 0):	
         
         grps = {}
         for i in range(1, len(res) - 4):
-    
-            grp = (res[i][1], res[i + 1][1], 
+            grpkey = (res[i][1], res[i + 1][1], 
                    res[i + 2][1], res[i + 3][1])
             
-            if grps.get(grp):
+            if grps.get(grpkey):
                 continue
-            grps[grp] = True
-            if mp.get(grp) is None:
+            
+            grps[grpkey] = True
+            if mp.get(grpkey) is None:
                 # 4 buyers, assume we need at least
                 # 3 entries. if idx is 2 and there
                 # are no entries, skip this step
                 if idx >= 2:
                     continue
-                mp[grp] = {}
+                mp[grpkey] = 0
             
-            key = res[i + 3][0]
-            if mp[grp].get(key) is None:
-                mp[grp][key] = 0
-
-            mp[grp][key] += 1
+            mp[grpkey] += res[i + 3][0]
             
-            
-        idx+=1
-    
-    max = 0
+    bananas = 0
     for grp in mp:
-        entries = mp[grp]
-        
-        if len(entries) < 1:
-            continue
-        
-        s = 0
-        for e in entries:
-            s += e * entries[e]
-        
-        if s > max:
-            max = s
-            
-    return max
+        bananas = max(bananas, mp[grp])
+                    
+    return bananas
 
 '''
 Helper
 '''
-
-def find_max(res: list):
-
-    m = 0
-    for tpl in res: 
-        m = max(m, tpl[1])
-    
-    return m
-
 def calc_secret(secret: int, steps: int, as_list = False):
 
-    l = [(int(str(secret)[-1]), 0)]
+    prev = secret % 10
+    l = [(prev, 0)]
     for i in range(0, steps):
-        val1 = secret * 64
-        val1 = mix(secret, val1)
-        val1 = prune(val1)
-
-        secret = val1
-        val2 = int(secret / 32)
-        val2 = mix(secret, val2)
-        val2 = prune(val2)
-
-        secret = val2
-        val3 = secret * 2048
-        val3 = mix(secret, val3)
-        secret = prune(val3)
+        secret = xor(secret, secret * 64) % 16777216
+        secret = xor(secret, int(secret / 32)) % 16777216
+        secret = xor(secret, secret * 2048) % 16777216
 
         if as_list:
-            last = int(str(secret)[-1])
-            diff = last - l[-1][0] 
+            last = secret % 10 
+            diff = last - prev 
             l.append((last, diff))
+            prev = last
 
 
     return l if as_list else secret
-
-
-def mix(secret: int, value: int):
-    return xor(secret, value)
-
-def prune(secret: int):
-    return secret % 16777216
-
-
